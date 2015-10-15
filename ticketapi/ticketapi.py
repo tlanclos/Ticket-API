285cd1b6cf98c420a5b7fcaa541f71a33c359024
Taylor Lanclos <tlanclos@live.com>
2015-10-14 21:46:42 -0500

    add some base comment blocks to ticketapi urls

--- ticketapi/ticketapi.py
+++ ticketapi/ticketapi.py
@@ -5,21 +5,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """
+    Home page
+    :return:
+    """
     return 'Welcome to Ticket-API!'
 
 
 @app.route('/login')
 def login():
+    """
+    Login page
+    :return:
+    """
     pass
 
 
 @app.route('/update-employee')
 def update_employee():
+    """
+    Update Employee page
+    :return:
+    """
     pass
 
 
 @app.route('/submit-ticket')
 def submit_ticket():
+    """
+    Submit Ticket page
+    :return:
+    """
     pass
 
 
