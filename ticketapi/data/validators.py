0dc1ec812c6e27374c8ee7211a13f73252d0a13d
Miranda Koubi <mrk3865@louisiana.edu>
2015-10-22 11:53:56 -0500

    Validator class.

--- ticketapi/data/validators.py
+++ ticketapi/data/validators.py
@@ -1,3 +1,28 @@
 import json
 from ticketapi.data.fields import *
 from ticketapi.data.response import *
+
+__all__ = [
+    'Validator'
+]
+
+
+class Validator(object):
+
+    fields = []
+
+    def __init__(self, current_request):
+        self.current_request = current_request
+
+    def validate(self, string_data):
+        try:
+            data = json.loads(string_data)
+
+            for i in self.fields:
+                if False in i.validate(data.get(i.name)):
+                    return FailureResponse()
+
+        except:
+            return FailureResponse()
+
+class EmployeeInfroValidator(Validator):
