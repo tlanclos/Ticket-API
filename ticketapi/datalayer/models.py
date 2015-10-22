08e504eefd32fbe526120045f2b8c334b82316b3
Taylor Lanclos <tlanclos@live.com>
2015-10-22 02:19:33 -0500

    reformat models with PEP8 coding style and use ticketapi's package level app

--- ticketapi/datalayer/models.py
+++ ticketapi/datalayer/models.py
@@ -1,7 +1,10 @@
-from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from ticketapi.apps import app
 
-app = Flask(__name__)
+
+__all__ = ['Company', 'Session', 'Ticket']
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlalchemy_example.db'
 db = SQLAlchemy(app)
 
@@ -12,6 +15,7 @@ class Company(db.Model):
     password = db.Column(db.String(16), unique=False, nullable=False)
     sessions = db.relationship('Session', backref='company', lazy='dynamic')
 
+
 class Session(db.Model):
     authKey = db.Column(db.String(36), primary_key=True)
     firstName = db.Column(db.String(32), unique=False, nullable=True)
@@ -21,6 +25,7 @@ class Session(db.Model):
     creationTime = db.Column(db.DateTime, unique=False, nullable=True)
     companyID = db.Column(db.String(64), db.ForeignKey('company.companyID'))
     tickets = db.relationship('Ticket', backref='session', lazy='dynamic')
+
 
 class Ticket(db.Model):
     ticketID = db.Column(db.Integer, primary_key=True)
