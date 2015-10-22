cc9badbc261c4159a6f33560e63ee88f4f5fd9c7
Taylor Lanclos <tlanclos@live.com>
2015-10-22 02:20:03 -0500

    fix coding style to comply with PEP8

--- ticketapi/datalayer/__session_wrapper.py
+++ ticketapi/datalayer/__session_wrapper.py
@@ -1,8 +1,13 @@
-from models import db, Company, Session, Ticket
+from ticketapi.datalayer.models import db
+
+
+__all__ = ['DB']
+
 
 class DB(object):
     def __enter__(self):
         return db.session
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type is not None:
             db.session.rollback()
