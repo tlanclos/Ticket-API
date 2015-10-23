1e0e1b7340c50ef6a286cee887c5cf9f4e9a828b
David Faulk <david.faulk94@gmail.com>
2015-10-22 23:29:49 -0500

    Logger tested.Outputs 9 rollbacks of 25mb

--- ticketapi/data/logger.py
+++ ticketapi/data/logger.py
@@ -5,28 +5,29 @@ import logging.config
 
 log_settings = {
     'version': 1,
-    'handlers': {
-        'file': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'level': 'INFO',
-            'formatter': 'detailed',
-            'filename': 'ticket-api.log',
-            'mode': 'a',
-            'maxBytes': 10485760,
-            'backupCount': 20,
-        },
-
-    },
     'formatters': {
         'detailed': {
             'format': '%(asctime)s [%(levelname)s] [%(module)s.%(funcName)s:%(lineno)d] - %(message)s',
             'datefmt': '%Y-%m-%d %H:%M:%S'
         },
+    },
+    'handlers': {
+        'file': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'INFO',
+            'formatter': 'detailed',
+            'filename': 'ticket_API.log',
+            'mode': 'a',
+            'maxBytes': 25600,
+            'backupCount': 9,
+        },
+
+    },
+
     'loggers': {
         'API-basic': {
             'level': 'INFO',
             'handlers': ['file']
-            }
         }
     }
 }
@@ -35,3 +36,11 @@ logging.config.dictConfig(log_settings)
 
 logger = logging.getLogger('API-basic')
 
+
+"""def main():
+    for i in range(50):
+        logger.info("test" + str(i))
+
+
+if __name__ == "__main__":
+    main()"""
