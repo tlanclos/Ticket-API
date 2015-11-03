f90839f56ba944a84bad3812d8d94503882c8054
Jensen <jensen@localhost.localdomain>
2015-11-03 13:38:01 -0500

    crypt: switch to using hash rather than encrypt/decrypt; docstrings

--- ticketapi/data/crypt.py
+++ ticketapi/data/crypt.py
@@ -5,10 +5,11 @@ import json
 __all__ = ['crypt']
 
 
-PEPPER_FILE = 'test-pepper.json'
-
-# maximum encrypt and decrypt time in seconds
-MAX_SCRYPT_TIME = 7
+class CryptConsts:
+    PEPPER_FILE = 'test-pepper.json'  # location of JSON pepper file
+    MAX_SCRYPT_TIME = 7  # maximum encrypt and decrypt time in seconds
+    HASH_BYTES = 128  # number of bytes for hash
+    SALT_BYTES = 128  # number of bytes for salt
 
 
 class CryptError(Exception):
@@ -20,11 +21,29 @@ class CryptError(Exception):
 
 
 class Crypt:
-    def __init__(self, pepper_loc, max_time):
-        self.__load_pepper(pepper_loc)
-        self.max_scrypt_time = max_time
+    """
+    Provides wrappers for scrypt that help in hashing and checking passwords with salt and pepper
+    """
+
+    def __init__(self):
+        """
+        Loads pepper from filesystem
+        """
+        self.__load_pepper(CryptConsts.PEPPER_FILE)
+
+    @staticmethod
+    def __get_salt():
+        """
+        :return: SALT_BYTES number of random bytes suitable for cryptographic use
+        """
+        return os.urandom(CryptConsts.SALT_BYTES)
 
     def __load_pepper(self, file_loc):
+        """
+        Loads pepper from file at file_loc and sets pepper attribute.
+        File should be JSON formatted with a 'pepper' key.
+        :param file_loc: file location to load pepper from
+        """
         try:
             with open(file_loc, 'r') as pepper_file:
                 try:
@@ -32,41 +51,48 @@ class Crypt:
                 except ValueError:
                     raise CryptError('Error initializing Crypt')
                 else:
-                    if pepper is None:
+                    if not pepper:
                         raise CryptError('Error initializing Crypt')
                     else:
                         self.pepper = bytes(pepper, encoding='utf8')
         except Exception:
             raise CryptError('Error initializing Crypt')
 
-    def check(self, hashed_pw, test_pw):
+    def check(self, test_pw, hashed_pw, salt):
+        """
+        Checks test_pw against hashed_pw by hashing with salt and pepper
+        :param test_pw: password to test
+        :param hashed_pw: hashed password
+        :param salt: salt
+        :return: whether test_pw matches hashed_pw when hashed with salt
+        """
         try:
-            scrypt.decrypt(hashed_pw, test_pw, maxtime=self.max_scrypt_time, encoding=None)
-            return True
+            test_hashed = scrypt.hash(test_pw, salt + self.pepper, buflen=CryptConsts.HASH_BYTES)
+            return test_hashed == hashed_pw
         except scrypt.error:
             return False
 
-    def encrypt(self, password):
+    def hash(self, password):
+        """
+        Hashes password with random salt and pepper and returns hashed password and salt
+        :param password: password to hash
+        :return: tuple of (hashed, salt) containing the hashed password and generated salt
+        """
         try:
-            salt = self.get_salt()
+            salt = self.__get_salt()
         except NotImplementedError:
             raise CryptError('Could not encrypt password')
 
-        hashed = scrypt.encrypt(salt + self.pepper, password)
+        hashed = scrypt.hash(password, salt + self.pepper, buflen=CryptConsts.HASH_BYTES)
 
         return hashed, salt
 
-    @staticmethod
-    def get_salt():
-        return os.urandom(128)
 
-
-crypt = Crypt(PEPPER_FILE, MAX_SCRYPT_TIME)
+crypt = Crypt()
 
 if __name__ == '__main__':
-    hashed, salt = crypt.encrypt('hereismypasswordhereismypassword')
-    is_correct = crypt.check(hashed, 'hereismypasswordhereismypassword')
+    hashed, salt = crypt.hash('hereismypassword')
+    print(hashed)
+    print(salt)
+    is_correct = crypt.check('hereismypassword', hashed, salt)
     print(is_correct)
-    is_correct = crypt.check(hashed, 'hereismypasswordhereismypassworD')
-    print(is_correct)
-
