44ded10728407764772eab8450ff04212cab89d7
Jensen <jensen@localhost.localdomain>
2015-11-04 15:41:23 -0500

    write authentication view

--- ticketapi/apps/ticketapi.py
+++ ticketapi/apps/ticketapi.py
@@ -1,8 +1,11 @@
+import json
 from flask import request
+from flask import jsonify
 from ticketapi.apps import app
 from ticketapi.data.decorators import *
 from ticketapi.data.validators import *
-
+from ticketapi.datalayer.procedures import *
+from ticketapi.data.response import FailureResponse
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -14,13 +17,31 @@ def home():
 
 
 @app.route('/login/', methods=['POST'], strict_slashes=False)
+@requires_validation(AuthInfoValidator)
 def login():
     """
     Login page
     :return:
     """
-    return 'Ticket-API login URI {data}'.format(data=request.data)
+    json_data = request.get_json(force=True)
 
+    try:
+        result = authenticate(**json_data)
+    except:
+        return FailureResponse(
+            error_code=408,
+            nice_message='There was an error authenticating you with the server',
+            debug_message='An exception occurred while trying to query the database'
+        )
+
+    if result is False:
+        return FailureResponse(
+            error_code=401,
+            nice_message='The company ID or password was incorrect, please try again',
+            debug_message='Invalid company ID or password'
+        ).response()
+
+    return jsonify(authKey=result)
 
 @app.route('/update-employee/', methods=['POST'], strict_slashes=False)
 def update_employee():
