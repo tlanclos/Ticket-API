c86d5bc2f05c8127f2fec84eac8ab045aedb3c88
Taylor Lanclos <tlanclos@live.com>
2015-11-05 22:05:29 -0600

    remove second level of decorator for requires_auth

--- ticketapi/data/decorators.py
+++ ticketapi/data/decorators.py
@@ -30,7 +30,7 @@ def requires_validation(validator):
     return decorator
 
 
-def requires_auth():
+def requires_auth(view):
     """
     Decorates a view to allow for authentication checking.  If authentication fails,
     the view will not be called--instead a failure response will be returned as
@@ -38,45 +38,43 @@ def requires_auth():
     If authentication is successful, the view will be called unaffected.
     :return: the requires authentication decorator
     """
-    def decorator(view):
-        def view_wrapper(*args, **kwargs):
-            nice_msg = 'There was an error authenticating you with the server'
+    def view_wrapper(*args, **kwargs):
+        nice_msg = 'There was an error authenticating you with the server'
 
-            # parse JSON
-            try:
-                json_data = request.get_json(force=True)
-            except BadRequest:
-                return FailureResponse(
-                    error_code=400,
-                    debug_message='Request body could not be parsed as JSON',
-                    nice_message=nice_msg
-                ).response()
+        # parse JSON
+        try:
+            json_data = request.get_json(force=True)
+        except BadRequest:
+            return FailureResponse(
+                error_code=400,
+                debug_message='Request body could not be parsed as JSON',
+                nice_message=nice_msg
+            ).response()
 
-            # test whether auth key is in JSON
-            auth_key = json_data.get('authKey')
-            if auth_key is None:
+        # test whether auth key is in JSON
+        auth_key = json_data.get('authKey')
+        if auth_key is None:
+            return FailureResponse(
+                error_code=401,
+                debug_message='JSON was missing authentication key',
+                nice_message=nice_msg
+            ).response()
+
+        try:
+            # check authentication
+            if check_auth(authKey=auth_key):
+                return view(*args, **kwargs)
+            else:
                 return FailureResponse(
                     error_code=401,
-                    debug_message='JSON was missing authentication key',
+                    debug_message='Authentication key is invalid',
                     nice_message=nice_msg
                 ).response()
+        except:
+            return FailureResponse(
+                error_code=520,
+                debug_message='Exception occurred when querying database.  Maybe the db is down',
+                nice_message=nice_msg
+            ).response()
 
-            try:
-                # check authentication
-                if check_auth(authKey=auth_key):
-                    return view(*args, **kwargs)
-                else:
-                    return FailureResponse(
-                        error_code=401,
-                        debug_message='Authentication key is invalid',
-                        nice_message=nice_msg
-                    ).response()
-            except:
-                return FailureResponse(
-                    error_code=520,
-                    debug_message='Exception occurred when querying database.  Maybe the db is down',
-                    nice_message=nice_msg
-                ).response()
-
-        return view_wrapper
-    return decorator
+    return view_wrapper
