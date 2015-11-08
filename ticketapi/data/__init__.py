fad979f08b237459fffc9118b3ed5f5917cdd299
John Patterson <jpp3911@louisiana.edu>
2015-11-07 18:03:26 -0600

    Added in connecting to Techneaux's MSSQL DB using SQLAlchemy's automap functionality. Should remain compatible with all existing code. Also changed the SETTINGS_FILE int data's __init__.py file to load and parse the settings.json file for global access.

--- ticketapi/data/__init__.py
+++ ticketapi/data/__init__.py
@@ -1,4 +1,7 @@
+import json
+
 __version__ = '0.0.1'
 __all__ = ['fields', 'response', 'logger', 'validators', 'decorators']
 
-SETTINGS_FILE = '/var/www/html/ticketapi/settings.json'
+
+SETTINGS_FILE = json.loads(open('/var/www/html/ticketapi/settings.json').read())
--- ticketapi/datalayer/models.py
+++ ticketapi/datalayer/models.py
@@ -1,47 +1,33 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine, MetaData, Table, Column, ForeignKey
+from urllib import parse
 from ticketapi.apps import app
-
+from ticketapi.data import SETTINGS_FILE
 
 __all__ = ['Company', 'Session', 'Ticket']
 
+connection_string = "DRIVER={{SQL Server}};Database={0};SERVER={1};UID={2};PWD={3}".format(
+    SETTINGS_FILE['db_name'],
+    SETTINGS_FILE['db_server'],
+    SETTINGS_FILE['db_username'],
+    SETTINGS_FILE['db_password']
+)
+connection_string = parse.quote_plus(connection_string)
+connection_string = "mssql+pyodbc:///?odbc_connect=%s" % connection_string
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///temp_db.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = connection_string
 db = SQLAlchemy(app)
 
+metadata = MetaData()
 
-class Company(db.Model):
-    """
-    Company table which holds company login information such as the pepper/salted/hashed password
-    """
-    companyID = db.Column(db.String(64), primary_key=True)
-    companyName = db.Column(db.String(64), unique=True, nullable=False)
-    password = db.Column(db.String(16), unique=False, nullable=False)
-    sessions = db.relationship('Session', backref='company', lazy='dynamic')
+metadata.reflect(db.engine, schema='ticketapi')
 
+Base = automap_base(metadata=metadata)
+Base.prepare()
 
-class Session(db.Model):
-    """
-    Session table which holds information about the employee. This will be used when an
-    employee updates his or her information such as name, email, and phone number
-    """
-    authKey = db.Column(db.String(36), primary_key=True)
-    firstName = db.Column(db.String(32), unique=False, nullable=True)
-    lastName = db.Column(db.String(32), unique=False, nullable=True)
-    email = db.Column(db.String(64), unique=False, nullable=True)
-    phoneNumber = db.Column(db.String(32), unique=False, nullable=True)
-    creationTime = db.Column(db.DateTime, unique=False, nullable=True)
-    companyID = db.Column(db.String(64), db.ForeignKey('company.companyID'))
-    tickets = db.relationship('Ticket', backref='session', lazy='dynamic')
+Company = Base.classes.Authentication
+Session = Base.classes.Session
+Ticket = Base.classes.Ticket
 
-
-class Ticket(db.Model):
-    """
-    Ticket table which holds information about a ticket submission such as the
-    location, photo, and description
-    """
-    ticketID = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(1024), unique=False, nullable=False)
-    location = db.Column(db.String(64), unique=False, nullable=True)
-    photo = db.Column(db.Text, unique=False, nullable=True)
-    creationTime = db.Column(db.DateTime, unique=False, nullable=False)
-    authKey = db.Column(db.String(36), db.ForeignKey('session.authKey'))
