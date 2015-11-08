3696fcc7ea5097eafd2c0200b9f4739ee7433ff1
Taylor Lanclos <tlanclos@live.com>
2015-11-07 20:38:53 -0600

    change SETTINGS_FILE from dictionary to string and make the dictionary called SETTINGS

--- ticketapi/data/__init__.py
+++ ticketapi/data/__init__.py
@@ -4,4 +4,7 @@ __version__ = '0.0.1'
 __all__ = ['fields', 'response', 'logger', 'validators', 'decorators']
 
 
-SETTINGS_FILE = json.loads(open('/var/www/html/ticketapi/settings.json').read())
+SETTINGS_FILE = '/var/www/html/ticketapi/settings.json'
+
+with open(SETTINGS_FILE, 'r') as settings:
+    SETTINGS = json.loads(settings.read())
--- ticketapi/datalayer/models.py
+++ ticketapi/datalayer/models.py
@@ -1,18 +1,17 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy import create_engine, MetaData, Table, Column, ForeignKey
+from sqlalchemy import MetaData
 from urllib import parse
 from ticketapi.apps import app
-from ticketapi.data import SETTINGS_FILE
+from ticketapi.data import SETTINGS
 
 __all__ = ['Company', 'Session', 'Ticket']
 
 connection_string = "DRIVER={{SQL Server}};Database={0};SERVER={1};UID={2};PWD={3}".format(
-    SETTINGS_FILE['db_name'],
-    SETTINGS_FILE['db_server'],
-    SETTINGS_FILE['db_username'],
-    SETTINGS_FILE['db_password']
+    SETTINGS['db_name'],
+    SETTINGS['db_server'],
+    SETTINGS['db_username'],
+    SETTINGS['db_password']
 )
 connection_string = parse.quote_plus(connection_string)
 connection_string = "mssql+pyodbc:///?odbc_connect=%s" % connection_string
@@ -30,4 +29,3 @@ Base.prepare()
 Company = Base.classes.Authentication
 Session = Base.classes.Session
 Ticket = Base.classes.Ticket
-
