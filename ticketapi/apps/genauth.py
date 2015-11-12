3ae464bdd42b841d5e7344b0e000f197fe93dba1
Taylor Lanclos <tlanclos@live.com>
2015-11-12 10:33:53 -0600

    genauth can run standalone cli without parametres

--- ticketapi/apps/genauth.py
+++ ticketapi/apps/genauth.py
@@ -6,14 +6,16 @@ from ticketapi.datalayer.procedures import add_auth
 
 if __name__ == '__main__':
     try:
+        # Create our argument parser
         parser = argparse.ArgumentParser(
             description='Tool to generate authorization rows and add them to the database based on a company name',
             epilog='\n'.join([
                 'I would have written a nice ncurses tui instead of this cli, but I figured someone would',
-                'eventually write a nice gui of some sort to add these rows in here :)'
+                'eventually write a nice gui of some sort to add rows in the Authentication table :)'
             ])
         )
 
+        # Add arguments to be parsed
         parser.add_argument(
             '--company', '-c',
             help='CompanyName from the Techneaux Company table'
@@ -29,29 +31,34 @@ if __name__ == '__main__':
 
         args = parser.parse_args()
 
-        if not all([args.company, args.username]):
-            exit(1)
+        # If we don't have a company or username in the arguments, ask for it
+        company = input('Enter company name: ').strip() if not args.company else args.company
+        username = input('Enter company username: ').strip() if not args.username else args.username
 
-        print('Adding company: "{company}"'.format(company=args.company))
-        print('   Username   : "{username}"'.format(username=args.username))
+        # Print the company and username being used
+        print('Adding company: "{company}"'.format(company=company))
+        print('   Username   : "{username}"'.format(username=username))
+
+        # If a password was not passed on the command line, ask for it
         if not args.password:
-            password = getpass.getpass(prompt='Password for {username}: '.format(username=args.username))
+            password = getpass.getpass(prompt='Password for {username}: '.format(username=username))
             if len(password) > 16:
                 print('Password too long, must be less than 16 characters')
                 exit(2)
 
-            confirm_password = getpass.getpass(prompt='Confirm password for {username}: '.format(username=args.username))
+            confirm_password = getpass.getpass(prompt='Confirm password for {username}: '.format(username=username))
             if password != confirm_password:
                 print('Passwords do not match')
                 exit(2)
         else:
             password = args.password
 
+        # Ask for confirmation
         cont = input('Continue (y/n)? ')
         if re.match(r'y(es)?', cont, re.IGNORECASE):
-            success, reason = add_auth(companyName=args.company, companyID=args.username, password=password)
+            success, reason = add_auth(companyName=company, companyID=username, password=password)
             if not success:
-                print('Failed to add authentication for "{company}"'.format(company=args.company))
+                print('Failed to add authentication for "{company}"'.format(company=company))
                 print(reason)
         else:
             print('User said no')
