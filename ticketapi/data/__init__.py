d0c920503c7a1e7bb8642146a0a67437ac29778e
Taylor Lanclos <tlanclos@live.com>
2015-11-17 18:06:32 -0600

    add package level variable for TICKET_API_ROOT

--- ticketapi/data/__init__.py
+++ ticketapi/data/__init__.py
@@ -1,10 +1,12 @@
+import os
 import json
 
 __version__ = '0.0.5'
 __all__ = ['fields', 'response', 'logger', 'validators', 'decorators']
 
 
-SETTINGS_FILE = '/var/www/html/ticketapi/settings.json'
+TICKET_API_ROOT = '/var/www/html'
+SETTINGS_FILE = os.path.join(TICKET_API_ROOT, 'ticketapi/settings.json')
 LOG_FILE = '/var/log/ticket-api/ticket-api.log'
 
 with open(SETTINGS_FILE, 'r') as settings:
