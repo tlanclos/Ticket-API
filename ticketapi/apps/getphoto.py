4f6f79202508cf2fc313c3232b595ed0e9955da2
Taylor Lanclos <tlanclos@live.com>
2015-11-17 18:07:02 -0600

    add comments and show error if ticket id not found

--- ticketapi/apps/getphoto.py
+++ ticketapi/apps/getphoto.py
@@ -1,14 +1,27 @@
+import os
 import argparse
-import sys
+from os.path import basename
 from ticketapi.datalayer import *
 from ticketapi.data.logger import logger
 from ticketapi.data import LOG_FILE
-from io import StringIO
+from ticketapi.data import TICKET_API_ROOT
+from io import BytesIO
 from PIL import Image
 
 
+# This is where we will store all photos called with --save
+PHOTO_LOCATION = os.path.join(TICKET_API_ROOT, 'saved_photos')
+
+
 def get_image(data):
-    stream = StringIO(data)
+    """
+    Convert byte string data to a PIL image
+
+    :param data: bytestring data to convert
+    :return: a PIL image
+    """
+    # get a byte stream of data (file descriptor to data)
+    stream = BytesIO(data)
     return Image.open(stream)
 
 
@@ -36,7 +49,7 @@ if __name__ == '__main__':
         metavar='filename.png',
         help=' '.join([
             'Save an image with a filename. This must be a filename and not filepath, the photo will be stored in',
-            'the ticketapi directory under saved_photos. If the directory does not exist, it will be created.'
+            '{location}. If the directory does not exist, it will be created.'.format(location=PHOTO_LOCATION)
         ])
     )
     parser.add_argument(
@@ -45,30 +58,37 @@ if __name__ == '__main__':
         help='Display the photo via ImageMagick, requires a display of some sort whether it be SSH X forwarding, etc.'
     )
 
-    if len(sys.argv) <= 1:
-        parser.print_help()
-        sys.exit(1)
-
+    # Parse the current command line arguments
     args = parser.parse_args()
 
     try:
         with DB() as s:
+            # Open a query for the selected ticket
             ticket = s.query(Ticket).filter(Ticket.ticketID == args.ticket_id).first()
             if ticket is not None:
+                # Here we take in the photo and get the PIL image associated with it
                 img_data = ticket.photo
                 img = get_image(img_data)
 
                 if args.save:
-                    img.save(args.save)
+                    # Since we're saving in an explicit directory we definitely have access to,
+                    # we get the basename of the passed in save location here and join it with the photo path
+                    path = os.path.join(PHOTO_LOCATION, basename(args.save))
+                    img.save(path)
+                    print('image saved to: {path}'.format(path=path))
 
                 if args.display:
                     try:
+                        # if we want to display the image then attempt to display it
                         img.show()
                     except Exception as e:
                         logger.exception(e)
                         print('Something went wrong, probably unable to open display, check logs: {logfile}'.format(
                             logfile=LOG_FILE
                         ))
+            else:
+                print('Ticket ID not found, please check parameters')
+                exit(2)
     except Exception as e:
         logger.exception(e)
         print('Something went wrong processing request, check logs: {logfile}'.format(logfile=LOG_FILE))
